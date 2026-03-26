580adc90fd8da20dc9a34aa40ab5673c95d8087b
duclaVietIS <ducla.viet.is@gmail.com>
2026-03-26 14:07:44 +0700

    [PC_VI] update table and sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
 -- Seed a dev user for login
 INSERT INTO users (id, username, email, password_hash, role, created_by, updated_by, failed_login_attempts, locked)
 VALUES ('00000000-0000-0000-0000-000000000001', 'admin', 'admin@example.com', '$2a$10$Gd1VW82G9G1NsoxgtSD6kO6xt3fa4QKSGeTSwyCuZktoewDPS0eeS', 'ADMIN', 'system', 'system', 0, 0);
+
+
+
+-- add full_name column to users table
+ALTER TABLE users ADD COLUMN full_name VARCHAR(255) AFTER username;
+-- add Bio column to users table
+ALTER TABLE users ADD COLUMN bio TEXT AFTER full_name;
+-- add profile_picture_url column to users table
+ALTER TABLE users ADD COLUMN profile_picture_url VARCHAR(255) AFTER bio;
